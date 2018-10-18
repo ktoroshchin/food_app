1db3834239b51323764fa02f8d210a8db18e57a2
Mikael Abehsera <Mikael.abehsera@gmail.com>
2018-10-18 14:05:27 -0400

    fixed async problem with index.js

--- routes/orders.js
+++ routes/orders.js
@@ -2,47 +2,61 @@
 const express = require('express');
 const router = express.Router();
 
+
+function generateRandomString() {
+  const possible = '1234567890qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM';
+  let randomID = '';
+  for (let i = 0; i < 10; i++) {
+    randomID = randomID + possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return randomID;
+}
+
 module.exports = (knex) => {
 
-  router.get('/:user_id', (req, res) => {
-    const userOrder = {
+  // router.get('/:user_id', (req, res) => {
+  //   const userOrder = {
 
-    }
+  //   }
 
-    res.redirect('order', userOrder);
-  });
+  //   res.redirect('order', userOrder);
+  // });
 
-  router.post('/order/:id', (req, res) => {
+  router.post('/orders', (req, res) => {
     const userURL = generateRandomString();
 
     const userInfo = [{
-      phone_number: req.params.phone_number, // placeholder name
+      phone_number: req.params.body.phone_number, // placeholder name
       shortURL: userURL
     }];
-
+    console.log(req.params.body);
+    
     knex('users')
       .insert(userInfo)
       .then(() => {
-        console.log('sucess')
+        console.log('sucess');
       })
       .catch((err) => {
         console.log(err);
-        throw err
+        throw err;
       })
       .finally(() => {
-        knex.destroy()
+        knex.destroy();
       });
 
-    knex
+    knex('users')
       .select('id')
-      .from('users')
       .where({
         shortURL: userURL
       })
       .then((rows) => {
-        const user_id = rows // console log to ensure rows is proper - possibly should be rows[0]
+        const user_id = rows;
+        console.log(rows); // console log to ensure rows is proper - possibly should be rows[0]
       })
-
+      .catch((err) => {
+        console.log(err);
+        throw err;
+      })
     // get food id & quantity with loop
 
     const userOrder = [];
@@ -57,7 +71,7 @@ module.exports = (knex) => {
     knex('order_details')
       .insert(userOrder)
       .then(() => {
-        console.log('sucess')
+        console.log('sucess');
       })
       .catch((err) => {
         console.log(err);
@@ -77,13 +91,5 @@ module.exports = (knex) => {
 }
 
 
-function generateRandomString() {
-  const possible = '1234567890qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM';
-  let randomID = '';
-  for (let i = 0; i < 10; i++) {
-    randomID = randomID + possible.charAt(Math.floor(Math.random() * possible.length));
-  }
-  return randomID;
-}
 
 
