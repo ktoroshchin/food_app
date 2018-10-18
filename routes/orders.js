008f5ff222fafee308336e9a6aec96f1612f0b22
char55 <haras.charlotte@gmail.com>
2018-10-18 15:27:53 +0000

    bug fix in loop position of post order

--- routes/orders.js
+++ routes/orders.js
@@ -12,6 +12,66 @@ module.exports = (knex) => {
     res.redirect('order', userOrder);
   });
 
+  router.post('/order/:id', (req, res) => {
+    const userURL = generateRandomString();
+
+    const userInfo = [{
+      phone_number: req.params.phone_number,
+      shortURL: userURL
+    }];
+
+    knex('users')
+      .insert(userInfo)
+      .then(() => {
+        console.log('sucess')
+      })
+      .catch((err) => {
+        console.log(err);
+        throw err
+      })
+      .finally(() => {
+        knex.destroy()
+      });
+
+    knex
+      .select('id')
+      .from('users')
+      .where({
+        shortURL: userURL
+      })
+      .then((rows) => {
+        const user_id = rows
+      })
+
+    // get food id & quantity with loop
+
+    const userOrder = [];
+    for (var i = 0; i < req.params.food_id.length; i++) {
+      userOrder.push({
+        food_id: req.params.food_id[i],
+        user_id: user_id,
+        quantity: req.params.quantity[i]
+      });
+    }
+
+    knex('order_details')
+      .insert(userOrder)
+      .then(() => {
+        console.log('sucess')
+      })
+      .catch((err) => {
+        console.log(err);
+        throw err
+      })
+      .finally(() => {
+        knex.destroy()
+      });
+
+    // TWILIO
+    // send SMS to user updating "order received"
+    // send SMS to admin/restaurant "order"
+
+  })
 
   return router;
 }
@@ -26,65 +86,4 @@ function generateRandomString() {
   return randomID;
 }
 
-router.post('/order/:id', (req, res) => {
-  const userURL = generateRandomString();
 
-  const userInfo = [{
-    phone_number: req.params.phone_number,
-    shortURL: userURL
-  }];
-
-  knex('users')
-    .insert(userInfo)
-    .then(() => {
-      console.log('sucess')
-    })
-    .catch((err) => {
-      console.log(err);
-      throw err
-    })
-    .finally(() => {
-      knex.destroy()
-    });
-
-  knex
-    .select('id')
-    .from('users')
-    .where({
-      shortURL: userURL
-    })
-    .then((rows) => {
-      const user_id = rows
-    })
-
-  // get food id & quantity with loop
-  //
-  const userOrder = [];
-  for (var i = 0; i < req.params.food_id.length; i++) {
-    userOrder.push({
-      food_id: req.params.food_id[i],
-      user_id: user_id,
-      quantity: req.params.quantity[i]
-    });
-
-  }
-
-
-  knex('order_details')
-    .insert(userOrder)
-    .then(() => {
-      console.log('sucess')
-    })
-    .catch((err) => {
-      console.log(err);
-      throw err
-    })
-    .finally(() => {
-      knex.destroy()
-    });
-
-  // TWILIO
-  // send SMS to user updating "order received"
-  // send SMS to admin/restaurant "order"
-
-})
