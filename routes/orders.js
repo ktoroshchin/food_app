a3c3ebd20ce329c655a112532f556fc3e7f71722
Mikael Abehsera <Mikael.abehsera@gmail.com>
2018-10-18 11:22:26 -0400

    fixed ejs error

--- routes/orders.js
+++ routes/orders.js
@@ -1,6 +1,6 @@
 'use strict';
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
 
 module.exports = (knex) => {
 
@@ -13,31 +13,36 @@ module.exports = (knex) => {
   });
 
 
-return router;
+  return router;
 }
 
 
 function generateRandomString() {
-  const possible  = '1234567890qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM';
+  const possible = '1234567890qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM';
   let randomID = '';
-  for(let i=0; i<10; i++){
+  for (let i = 0; i < 10; i++) {
     randomID = randomID + possible.charAt(Math.floor(Math.random() * possible.length));
   }
   return randomID;
 }
 
-router.post('/', (req, res) => {
+router.post('/order/:id', (req, res) => {
   const userURL = generateRandomString();
 
   const userInfo = [{
-    phone_number : req.params.phone_number,
-    shortURL : userURL
+    phone_number: req.params.phone_number,
+    shortURL: userURL
   }];
 
   knex('users')
     .insert(userInfo)
-    .then(() => { console.log('sucess')})
-    .catch((err) => { console.log(err); throw err})
+    .then(() => {
+      console.log('sucess')
+    })
+    .catch((err) => {
+      console.log(err);
+      throw err
+    })
     .finally(() => {
       knex.destroy()
     });
@@ -45,35 +50,41 @@ router.post('/', (req, res) => {
   knex
     .select('id')
     .from('users')
-    .where({shortURL: userURL})
+    .where({
+      shortURL: userURL
+    })
     .then((rows) => {
       const user_id = rows
     })
 
-// get food id & quantity with loop
-//
-const userOrder = [];
-for(var i=0; i<req.params.food_id.length; i++) {
-  userOrder.push({
-    food_id: req.params.food_id[i],
-    user_id : user_id,
-    quantity: req.params.quantity[i]
-  });
+  // get food id & quantity with loop
+  //
+  const userOrder = [];
+  for (var i = 0; i < req.params.food_id.length; i++) {
+    userOrder.push({
+      food_id: req.params.food_id[i],
+      user_id: user_id,
+      quantity: req.params.quantity[i]
+    });
 
-}
+  }
 
 
   knex('order_details')
     .insert(userOrder)
-    .then(() => { console.log('sucess')})
-    .catch((err) => { console.log(err); throw err})
+    .then(() => {
+      console.log('sucess')
+    })
+    .catch((err) => {
+      console.log(err);
+      throw err
+    })
     .finally(() => {
       knex.destroy()
     });
 
-    // TWILIO
-    // send SMS to user updating "order received"
-    // send SMS to admin/restaurant "order"
+  // TWILIO
+  // send SMS to user updating "order received"
+  // send SMS to admin/restaurant "order"
 
 })
-
