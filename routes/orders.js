50dde45669ee4ff9a7c4c290901bd630e81396ed
Mikael Abehsera <Mikael.abehsera@gmail.com>
2018-10-18 17:27:59 -0400

    pushing broken order.js

--- routes/orders.js
+++ routes/orders.js
@@ -12,86 +12,95 @@ function generateRandomString() {
 }
 
 module.exports = (knex) => {
-  router.get('/:shortURL', (req, res) => {
-    knex
-      .select('*')
-      .from('users')
-      .innerJoin('order_details','users.id','order_details.user_id')
-      .innerJoin('food_items', 'order_details.food_id', 'food_items.id')
-      .where({
-        shortURL: req.params.shortURL
-      })
-      .then((userOrder) => {
-        res.render('orders', { userOrder })
-        })
-        .catch((err) => {
-          console.log(err);
-          throw err;
-        })
-        .finally(() => {
-          knex. destroy();
-        });
-      });
 
-  router.post('/', (req, res) => {
-    const userURL = generateRandomString();
 
-    const userInfo = [{
-      phone_number: req.params.body.phone_number, // placeholder name
-      shortURL: userURL
-    }];
-    console.log(req.params.body);
+    router.post('/', (req, res) => {
+          const userURL = generateRandomString();
 
-    knex('users')
-      .insert(userInfo)
-      .then(() => {
-        console.log('sucess');
-      })
-      .catch((err) => {
-        console.log(err);
-        throw err;
-      })
-      .finally(() => {
-        knex.destroy();
-      });
+          const userInfo = [{
+            phone_number: req.body.phone, // placeholder name
+            shortURL: userURL
+          }];
+          
+          const userOrder = [];
+          for (var i = 0; i < req.params.food_id.length; i++) {
+            userOrder.push({
+              food_id: req.params.food_id[i], //placeholder
+              user_id: user_id,
+              quantity: 1 //req.params.quantity[i] //placeholder
+            });
+          }
 
-    knex('users')
-      .select('id')
-      .where({
-        shortURL: userURL
-      })
-      .then((rows) => {
-        const user_id = rows;
-        console.log(rows); // console log to ensure rows is proper - possibly should be rows[0]
-      })
-      .catch((err) => {
-        console.log(err);
-        throw err;
-      })
-    // get food id & quantity with loop
+          knex('users')
+            .insert(userInfo)
+            .returning('id')
+            .then((id) => {
+              knex('order_detail')
+              .insert(userOrder)
+                .then(() => {
+                  console.log('success');
+                })
+                .catch((err) => {
+                  console.log(err);
+                  throw err;
+                })
+                .finally(() => {
+                  res.render('/:' + userURL);
+                });
 
-    const userOrder = [];
-    for (var i = 0; i < req.params.food_id.length; i++) {
-      userOrder.push({
-        food_id: req.params.food_id[i], //placeholder
-        user_id: user_id,
-        quantity: 1 //req.params.quantity[i] //placeholder
-      });
-    }
+            })
 
-  knex('order_details')
-    .insert(userOrder)
-    .then(() => {
-      console.log('sucess')
-    })
-    .catch((err) => {
-      console.log(err);
-      throw err
-    })
-    .finally(() => {
-      knex.destroy()
-      res.render("/:" + userURL);
-    });
-});
-return router;
-}
+
+          knex('users')
+            .insert(userInfo)
+            .then(() => {
+              console.log('success');
+            })
+            .catch((err) => {
+              console.log(err);
+              throw err;
+            })
+            .finally(() => {});
+
+          knex('users')
+            .select('id')
+            .where({
+              shortURL: userURL
+            })
+            .then((rows) => {
+              const user_id = rows;
+              console.log('ROWS ROWS ROWS: ', rows[0]); // console log to ensure rows is proper - possibly should be rows[0]
+            })
+            .catch((err) => {
+              console.log(err);
+              throw err;
+            });
+          // get food id & quantity with loop
+
+          
+
+
+          router.get('/:shortURL', (req, res) => {
+            knex('users')
+              .select('*')
+              .innerJoin('order_details', 'users.id', 'order_details.user_id')
+              .innerJoin('food_items', 'order_details.food_id', 'food_items.id')
+              .where({
+                shortURL: req.params.shortURL
+              })
+              .then((userOrder) => {
+                res.render('orders', {
+                  userOrder
+                });
+              })
+              .catch((err) => {
+                console.log(err);
+                throw err;
+              })
+              .finally(() => {
+                knex.destroy();
+              });
+          });
+
+          return router;
+        };
